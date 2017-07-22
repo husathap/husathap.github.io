e3ade43574718f574c2e118e96d5503775419bc8
sathaporn.hu <sathaporn.hu@ucalgary.ca>
2017-07-21 21:14:32 -0600

    Additional update

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 let educationModel = [
   { name: "University of Calgary", degree: "Master of Science", program: "Computer Science", start: "2015-Sep", end: "Present", color: "red", locationImg: "ucalgary.jpg"},
-  { name: "Dalhousie University", degree: "Visiting (MSc)", program: "NICE RADIANT", start: "2016-Aug", color: "#FFD700", locationImg: "dalhousie.jpg"},
+  //{ name: "Dalhousie University", degree: "Visiting (MSc)", program: "NICE RADIANT", start: "2016-Aug", color: "#FFD700", locationImg: "dalhousie.jpg"}, 
   { name: "University of Toronto", degree: "Bachelor of Science", program: "Computer Science (Specialist), Cognitive Science (Major), French (Minor)", start: "2011-Sep", end: "2015-May", color: "#00204E", locationImg: "uoft.jpg"}
 ];
 
@@ -17,13 +17,13 @@ let teachingModel = [
 ];
 
 let projectModel = [
-  { name: "Tangra", description: "Web portal for online experiment", link: "http://www.taglab.toronto.edu/index.php/projects/tangra-defining-and-supporting-online-studies-and-randomized-controlled-trials/"},
-  { name: "InTouch", description: "Mobile communcations app for senior", link: "http://taglab.utoronto.ca/index.php/projects/intouch-connections-technologies-to-combat-social-isolation-and-loneliness/"},
-  { name: "ALLT", description: "Mobile eBook reader", link: "http://www.taglab.toronto.edu/index.php/projects/simple-project/"},
+  { name: "Caracas", description: "YouTube video player for language learning"},
+  { name: "D-Plot", description: "Bigram visualization tool"},
   { name: "Project Happiness", description: "Digital physical installation"},
   { name: "Part-of-Speech Visualization", description: "A HTML-based part-of-speech visualization"},
-  { name: "D-Plot", description: "Bigram visualization tool"},
-  { name: "Caracas", description: "YouTube video player for language learning"}
+  { name: "InTouch", description: "Mobile communcations app for senior", link: "http://taglab.utoronto.ca/index.php/projects/intouch-connections-technologies-to-combat-social-isolation-and-loneliness/"},
+  { name: "ALLT", description: "Mobile eBook reader", link: "http://www.taglab.toronto.edu/index.php/projects/simple-project/"},
+  { name: "Tangra", description: "Web portal for online experiment", link: "http://www.taglab.toronto.edu/index.php/projects/tangra-defining-and-supporting-online-studies-and-randomized-controlled-trials/"},
 ];
 
 let jobModel = [
@@ -33,8 +33,8 @@ let jobModel = [
 ];
 
 let volunteerModel = [
-  { name: "Language Exchange Participant", organization: "Japan-Canada Student Association", affiliation: "University of Toronto", start: "Sep-2011", end: "May-2014"},
-  { name: "Administrator", organization: "Cognitive Science and Artificial Intelligence Student Association", affiliation: "University of Toronto", start: "Sep-2012", end: "May-2015"},
+  { name: "Vice President - Finance", organization: "Computer Science Graduate Society", affiliation: "University of Calgary", start: "May-2016", end: "Aug-2016"},
   { name: "Fire Warden", organization: "Crowsnest Hall Residence", affiliation: "University of Calgary", start: "Sep-2015", end: "Dec-2015"},
-  { name: "Vice President - Finance", organization: "Computer Science Graduate Society", affiliation: "University of Calgary", start: "May-2016", end: "Aug-2016"}
+  { name: "Administrator", organization: "Cognitive Science and Artificial Intelligence Student Association", affiliation: "University of Toronto", start: "Sep-2012", end: "May-2015"},
+  { name: "Language Exchange Participant", organization: "Japan-Canada Student Association", affiliation: "University of Toronto", start: "Sep-2011", end: "May-2014"},
 ];
