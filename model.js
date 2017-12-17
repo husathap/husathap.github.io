741c5eabe346af0762003b1f4056db032e5eeb9d
sathaporn.hu <sathaporn.hu@ucalgary.ca>
2017-12-17 11:35:31 -0700

    Updated the model

--- model.js
+++ model.js
@@ -1,8 +1,8 @@
 let educationModel = [
-	{ university: "Dalhousie University", degree: "Doctor of Philosophy", program: "Computer Science", start: "2018-Jan", end: "Present", locationImg: "dalhousie.jpg"},
-  { university: "University of Calgary", degree: "Master of Science", program: "Computer Science", start: "2015-Sep", end: "2017-Dec", color: "red", locationImg: "ucalgary.jpg"},
-  //{ name: "Dalhousie University", degree: "Visiting (MSc)", program: "NICE RADIANT", start: "2016-Aug", color: "#FFD700", locationImg: "dalhousie.jpg"}, 
-  { university: "University of Toronto", degree: "Bachelor of Science", program: "Computer Science (Specialist), Cognitive Science (Major), French (Minor)", start: "Sep-2011", end: "May-2015", color: "#00204E", locationImg: "uoft.jpg"}
+  { university: "Dalhousie University", degree: "Doctor of Philosophy", program: "Computer Science", start: "Jan 2018", end: "Present", locationImg: "dalhousie.jpg"},
+  { university: "University of Calgary", degree: "Master of Science", program: "Computer Science", start: "Sep 2015", end: "Dec 2017", color: "red", locationImg: "ucalgary.jpg"},
+  { university: "Dalhousie University", degree: "Visiting", program: "NICE RADIANT", start: "Aug 2016", end: "Aug 2016", color: "#FFD700", locationImg: "dalhousie.jpg"}, 
+  { university: "University of Toronto", degree: "Bachelor of Science", program: "Computer Science (Specialist), Cognitive Science (Major), French (Minor)", start: "Sep 2011", end: "May 2015", color: "#00204E", locationImg: "uoft.jpg"}
 ];
 
 let scholarshipModel = [
@@ -29,14 +29,14 @@ let projectModel = [
 ];
 
 let privateModel = [
-  { role: "Research Intern", company: "Lenovo", location: "Beijing, China", description: "Analyzed online data. Conducted experiment with virtual reality.", start: "Sep-2016", end: "Dec-2016", color: "red", locationImg: "beijing.jpg"},
-  //{ name: "Research Assistant/Trainee", company: "TAGLab, University of Toronto", location: "Toronto, Canada", description: "Tested, developed software. Assisted with research", start: "Feb-2015", end: "Aug-2015", color: "#00204E", locationImg: "toronto.jpg"},
-  { role: "Information Technology Intern", company: "Jet Asia Airways", location: "Bangkok, Thailand", description: "Maintained the IT structure. Implementing a new infrastructure", start: "Jun-2014", end: "Aug-2014", color: "white", locationImg: "bangkok.jpg"},
+  { role: "Research Intern", company: "Lenovo", location: "Beijing, China", description: "Analyzed online data. Conducted experiment with virtual reality.", start: "Sep 2016", end: "Dec 2016", color: "red", locationImg: "beijing.jpg"},
+  //{ name: "Research Assistant/Trainee", company: "TAGLab, University of Toronto", location: "Toronto, Canada", description: "Tested, developed software. Assisted with research", start: "Feb 2015", end: "Aug 2015", color: "#00204E", locationImg: "toronto.jpg"},
+  { role: "Information Technology Intern", company: "Jet Asia Airways", location: "Bangkok, Thailand", description: "Maintained the IT structure. Implementing a new infrastructure", start: "Jun 2014", end: "Aug 2014", color: "white", locationImg: "bangkok.jpg"},
 ];
 
 let volunteerModel = [
-  { role: "Vice President - Finance", organization: "Computer Science Graduate Society", affiliation: "University of Calgary", start: "Sep-2016", end: "Aug-2016"},
-  { role: "Fire Warden", organization: "Crowsnest Hall Residence", affiliation: "University of Calgary", start: "Sep-2015", end: "Dec-2015"},
-  { role: "Administrator", organization: "Cognitive Science and Artificial Intelligence Student Association", affiliation: "University of Toronto", start: "Sep-2012", end: "May-2015"},
-  { role: "Language Exchange Participant", organization: "Japan-Canada Student Association", affiliation: "University of Toronto", start: "Sep-2011", end: "May-2014"},
+  { role: "Vice President - Finance", organization: "Computer Science Graduate Society", affiliation: "University of Calgary", start: "Sep 2016", end: "Aug 2016"},
+  { role: "Fire Warden", organization: "Crowsnest Hall Residence", affiliation: "University of Calgary", start: "Sep 2015", end: "Dec 2015"},
+  { role: "Administrator", organization: "Cognitive Science and Artificial Intelligence Student Association", affiliation: "University of Toronto", start: "Sep 2012", end: "May 2015"},
+  { role: "Language Exchange Participant", organization: "Japan-Canada Student Association", affiliation: "University of Toronto", start: "Sep 2011", end: "May 2014"},
 ];
