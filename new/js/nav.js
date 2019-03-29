a440a0c076d85fac137fc03df798621b47c4cbdc
Hubert Hu <hu.hubert@live.ca>
2019-03-29 15:44:56 -0300

    fixed width issue.

--- new/js/nav.js
+++ new/js/nav.js
@@ -13,14 +13,15 @@ $("#hide_a").click(function() {
 function adjustScaffold() {
 	var scaffold = $("#scaffold");
 	var nav = $("nav");
-	var nav_width = nav.css("width");
 	
 	if (nav.css("display") == "none") {
 		var hamburger = $("#hamburger");
 		var hamburger_height = hamburger.css("height");
 		scaffold.css({"left": "0", "top": "3.5rem"});
+		scaffold.css({"width": "100%"});
 	} else {
-		scaffold.css({"left": nav_width, "top": "0"});
+		scaffold.css({"left": nav.css("width"), "top": "0"});
+		scaffold.css({"width": $(window).width() - nav.width() + "px"});
 	}
 }
 
