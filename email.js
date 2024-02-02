405d04e39f20e66df0903d66d39beb0f49353f07
Hubert (Sathaporn) Hu <hu.hubert@live.ca>
2024-02-01 23:43:51 -0500

    New temporary website.

--- email.js
+++ email.js
@@ -1,8 +1,8 @@
 // Anti-scraping idea from: https://www.matthewthom.as/blog/stop-email-scraping/
 
-let acct = "hs.hu";
-let dom = "dal.ca";
+let acct = "hus";
+let dom = "algomau.ca";
 
 let email_element = document.getElementById("email");
-email_element.innerHTML = `${acct}@${dom}`;
+email_element.innerHTML = `<i class="bi bi-envelope-fill" aria-hidden="true"></i> Email</a>: ${acct}@${dom}`;
 email_element.setAttribute("href", `mailto:${acct}@${dom}`);
