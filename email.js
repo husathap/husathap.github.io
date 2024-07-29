a2b6e5cc2921da075c1953412f4a29c6bd783a57
Hubert (Sathaporn) Hu <hu.hubert@live.ca>
2024-07-29 15:15:07 -0400

    Updated new personal website.

--- email.js
+++ email.js
@@ -4,5 +4,5 @@ let acct = "hus";
 let dom = "algomau.ca";
 
 let email_element = document.getElementById("email");
-email_element.innerHTML = `<i class="bi bi-envelope-fill" aria-hidden="true"></i> Email</a>: ${acct}@${dom}`;
+email_element.innerHTML = `<i class="bi bi-envelope-at-fill" aria-hidden="true"></i> Email</a>: ${acct}@${dom}`;
 email_element.setAttribute("href", `mailto:${acct}@${dom}`);
